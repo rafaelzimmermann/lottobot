24cfad00a8105637b21f75c809ad4c6df6503d3b
Rafael Zimmermann <rafael.zimmermann@trivago.com>
2021-11-04 19:38:34 +0100

    Add mega sena

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ async def async_main(config):
     telegram = Telegram(config['telegram']['token'])
     timer = asyncio.get_event_loop().time
 
+    def print_sena(chat_id: int):
+        numbers = []
+        r = random.Random()
+        while len(numbers) < 6:
+            n = r.randint(1, 60)
+            if n not in numbers:
+                numbers.append(n)
+
+        message = f"Numbers: {sorted(numbers)}"
+        telegram.send(chat_id, message)
+
     def print_numbers(chat_id: int):
         numbers = []
         r = random.Random()
@@ -26,6 +37,8 @@ async def async_main(config):
             message = u['message']
             if '/lotto' in message['text']:
                 print_numbers(message['chat']['id'])
+            if '/sena' in message['text']:
+                print_sena(message['chat']['id'])
 
     interval = 10  # seconds
 
